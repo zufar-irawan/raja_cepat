8ffc2382ff211920cd7b12b1daadf1ca90b214cc
irawanrafidzufar <irawanrafidzufar@gmail.com>
2025-07-09 14:03:41 +0700

    header

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,43 @@
+"use client"
+
+import Image from "next/image"
+import Link from "next/link"
+import { useEffect, useState } from "react"
+
 const Header = () => {
+    const [isScrolled, setIsScrolled] = useState(false)
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsScrolled(window.scrollY > 80)
+        }
+
+        window.addEventListener("scroll", handleScroll)
+        return () => window.removeEventListener("scroll", handleScroll)
+    }, [])
+
     return (
-        <header>
-            Hello World
+        <header className="fixed w-full">
+            <div className="w-full relative bg-white py-5 z-50">
+                <Image
+                    src={"/images/logo.png"}
+                    alt="Logo"
+                    width={80}
+                    height={80}
+                    className="mx-auto"
+                />
+            </div>
+
+            <div className={`
+                p-3 bg-gray-200 w-full absolute z-10 transition-all delay-100
+                ${isScrolled ? 'top-22' : 'top-0'} ease-in-out
+            `}>
+                <ul className="flex justify-center w-[50%] mx-auto gap-8">
+                    <li className="text-gray-800 hover:text-blue-600"><Link href={"#"}>Beranda</Link></li>
+                    <li className="text-gray-800 hover:text-blue-600"><Link href={"#"}>Tentang</Link></li>
+                    <li className="text-gray-800 hover:text-blue-600"><Link href={"#"}>Kontak</Link></li>
+                </ul>
+            </div>
         </header>
     )
 }
